9e85ee879c8cf9789f80fc5b0412411f71cc98d6
Shruthi Nagaraju <mnkshruthi@outlook.com>
2024-01-23 13:06:37 -0500

    Add alsp_trips theme

---
+++ web/themes/custom/alps_trips/js/alps-trips.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * alps_trips behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.alpsTrips = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
